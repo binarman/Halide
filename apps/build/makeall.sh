8da6c49496ef4dc4ff52c32ec1e0e782817ef193
binarman <efimov.alexander@gmail.com>
2021-01-13 14:04:42 +0300

    add files with build hints

---
+++ apps/build/makeall.sh
@@ -0,0 +1,2 @@
+Halide_DIR=../../install cmake -DCMAKE_BUILD_TYPE=Release ../
+make -j8
---
+++ build/makeall.sh
@@ -0,0 +1,4 @@
+export LLVM_ROOT=/mnt/linuxStorage/llvm/install
+export LLVM_CONFIG=$LLVM_ROOT/bin/llvm-config
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=~/ops/codegen_experiments/halide/Halide/install/ ../
+make -j8 install
